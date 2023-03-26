f93a1895580d2e0ac5e159b3c492741506954baa
HeroutRozilyo <heroot12@gmail.com>
2023-03-26 14:17:23 +0300

    send again

---
+++ src/KeyBoardFull.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import '../src/css/KeyBoard.css';
+import {EnglishKey,HebrewKeys,EmojiKeys} from './KeyBords.js'
+
+ export class KeyBoardEBE extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: '',
+      isEngKeyBoard: true,
+      isEmojiKeyboard: false,
+      isCapsLockOn:false,
+      virtualKeys:EnglishKey,
+      direction: 'ltr' // or 'rtl'
+    };
+
+    
+
+    
+
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+    
+  }
+
+  handleKeyPress(key) {
+    let newText = this.state.text;
+
+    switch (key) {
+      case 'backspace':
+        newText = newText.slice(0, -1);
+        break;
+      case 'enter':
+        newText += '\n';
+        break;
+      case 'tab':
+        newText += '  ';
+        break;
+      case 'capsLock':
+        this.setState({isCapsLockOn:!this.state.isCapsLockOn});
+        
+        break;
+      case 'shift':
+        // do nothing
+        break;
+      case 'space':
+        newText += ' ';
+        break;
+        case 'Hebrew':
+           this.setState({virtualKeys:HebrewKeys});
+            this.setState({isEngKeyBoard:!this.state.isEngKeyBoard});
+            if(this.state.isEmojiKeyboard){
+                this.setState({isEmojiKeyboard:!this.state.isEmojiKeyboard});
+            }
+            break;
+        
+        case 'English':
+            this.setState({virtualKeys:EnglishKey});
+            this.setState({
+                isEngKeyBoard:!this.state.isEngKeyBoard
+                });
+                if(this.state.isEmojiKeyboard){
+                    this.setState({isEmojiKeyboard:!this.state.isEmojiKeyboard});
+                }
+            break;
+        
+        case '😀':
+            this.setState({isEmojiKeyboard:!this.state.isEmojiKeyboard});
+            break;
+        case '←':
+            this.setState({direction:'rtl'});
+            break;
+        case '→':
+            this.setState({direction:'ltr'});
+            break;
+
+      default:
+        if(this.state.isCapsLockOn){ newText+=key.toUpperCase();}
+         else newText += key;
+       
+    }
+
+    this.setState({
+      text: newText,
+    });
+  }
+
+  
+
+  render() {
+    return (
+      <div className="App">
+        <textarea className="text-editor" dir={this.state.direction}  value={this.state.text} onChange={(e) => this.setState({ text: e.target.value })}></textarea>
+<div className="keyboard">
+{this.state.isEmojiKeyboard ? (
+<div className="emoji-keyboard">
+{EmojiKeys.map((keyRow,rowIndex)=><div key={rowIndex} className="key-row">
+    {keyRow.map((emoji) => (
+<button key={emoji} onClick={() => this.handleKeyPress(emoji)}>
+{emoji}
+</button>
+))}
+</div>
+)}
+</div>) : (
+<div className={this.state.isEngKeyBoard ? 'english-keyboard' : 'hebrew-keyboard'}>
+{this.state.virtualKeys.map((keyRow, rowIndex) => (
+<div key={rowIndex} className="key-row">
+{keyRow.map((key) => (
+<button key={key} onClick={() => this.handleKeyPress(key)}>
+{key}
+</button>
+))}
+</div>
+))}
+</div>
+)}
+
+</div>
+</div>
+
+);
+}
+}
--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,126 +1,28 @@
+
 import React from 'react';
-import '../src/css/KeyBoard.css';
-import {EnglishKey,HebrewKeys,EmojiKeys} from '../src/KeyBords.js'
+import {KeyBoardEBE} from './KeyBoardFull.js'
+ export default class  TextEditorFull extends React.Component {
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      text: '',
-      isEngKeyBoard: true,
-      isEmojiKeyboard: false,
-      isCapsLockOn:false,
-      virtualKeys:EnglishKey,
-      direction: 'ltr' // or 'rtl'
-    };
-
-    
-
-    
-
-    this.handleKeyPress = this.handleKeyPress.bind(this);
-    
-  }
-
-  handleKeyPress(key) {
-    let newText = this.state.text;
-
-    switch (key) {
-      case 'backspace':
-        newText = newText.slice(0, -1);
-        break;
-      case 'enter':
-        newText += '\n';
-        break;
-      case 'tab':
-        newText += '  ';
-        break;
-      case 'capsLock':
-        this.setState({isCapsLockOn:!this.state.isCapsLockOn});
+    // constructor(props) {
+    //     super(props);
         
-        break;
-      case 'shift':
-        // do nothing
-        break;
-      case 'space':
-        newText += ' ';
-        break;
-        case 'Hebrew':
-           this.setState({virtualKeys:HebrewKeys});
-            this.setState({isEngKeyBoard:!this.state.isEngKeyBoard});
-            if(this.state.isEmojiKeyboard){
-                this.setState({isEmojiKeyboard:!this.state.isEmojiKeyboard});
-            }
-            break;
-        
-        case 'English':
-            this.setState({virtualKeys:EnglishKey});
-            this.setState({
-                isEngKeyBoard:!this.state.isEngKeyBoard
-                });
-                if(this.state.isEmojiKeyboard){
-                    this.setState({isEmojiKeyboard:!this.state.isEmojiKeyboard});
-                }
-            break;
-        
-        case '😀':
-            this.setState({isEmojiKeyboard:!this.state.isEmojiKeyboard});
-            break;
-        case '←':
-            this.setState({direction:'rtl'});
-            break;
-        case '→':
-            this.setState({direction:'ltr'});
-            break;
+    // }
+    render() {
+        return(
+           <div>
 
-      default:
-        if(this.state.isCapsLockOn){ newText+=key.toUpperCase();}
-         else newText += key;
-       
+           
+          <KeyBoardEBE>
+
+          </KeyBoardEBE>
+          <div> <h1>tryyyy</h1></div>
+          </div> 
+                
+            
+        )
+
+        
+        
     }
-
-    this.setState({
-      text: newText,
-    });
-  }
-
-  
-
-  render() {
-    return (
-      <div className="App">
-        <textarea className="text-editor" dir={this.state.direction}  value={this.state.text} onChange={(e) => this.setState({ text: e.target.value })}></textarea>
-<div className="keyboard">
-{this.state.isEmojiKeyboard ? (
-<div className="emoji-keyboard">
-{EmojiKeys.map((keyRow,rowIndex)=><div key={rowIndex} className="key-row">
-    {keyRow.map((emoji) => (
-<button key={emoji} onClick={() => this.handleKeyPress(emoji)}>
-{emoji}
-</button>
-))}
-</div>
-)}
-</div>) : (
-<div className={this.state.isEngKeyBoard ? 'english-keyboard' : 'hebrew-keyboard'}>
-{this.state.virtualKeys.map((keyRow, rowIndex) => (
-<div key={rowIndex} className="key-row">
-{keyRow.map((key) => (
-<button key={key} onClick={() => this.handleKeyPress(key)}>
-{key}
-</button>
-))}
-</div>
-))}
-</div>
-)}
-
-</div>
-</div>
-
-);
 }
-}
-
-export default App;
+//export default TextEditorFull ;
